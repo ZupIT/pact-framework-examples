a6a4675b146893284746c4ee5bdc4978d5129138
jankrejcizup <jan.krejci@zup.com.br>
2020-08-25 16:34:37 -0300

    Changes on jobs

---
+++ example/ci/jenkins/jenkins_config/jobs.groovy
@@ -0,0 +1,30 @@
+def participants = [
+    [
+        name: "client-api",
+        description: "Client microservice",
+        jenkinsfilePath: "example/java/spring-boot/one_consumer_one_provider/client-api/Jenkinsfile",
+        repository: {
+            git {
+                id("consumer-sample")
+                remote("https://github.com/ZupIT/dtp-pact-examples")
+                credentialsId("githubCredentials")
+            }
+        }
+    ]
+]
+
+participants.each { service ->
+    println("Criando multipipeline for:: $service.name")
+
+    multibranchPipelineJob("${service.name}") {
+        description(service.description)
+
+        branchSources service.repository
+
+        factory {
+            workflowBranchProjectFactory {
+                scriptPath(service.jenkinsfilePath)
+            }
+        }
+    }
+}
