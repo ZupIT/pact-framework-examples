11fcb82527d609a3d18695162ffde88471e5bbe3
Hader Araujo <hader.araujo@zup.com.br>
2020-08-26 09:42:41 -0300

    br.com.zup.pact.provider

--- example/java/spring-boot/one_consumer_one_provider/consumer/src/test/java/br/com/zup/pact/consumer/ConsumerApplicationTests.java
+++ example/java/spring-boot/one_consumer_one_provider/consumer/src/test/java/br/com/zup/pact/consumer/ConsumerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ClientApiApplicationTests {
+class ConsumerApplicationTests {
 
     @Test
     void contextLoads() {
