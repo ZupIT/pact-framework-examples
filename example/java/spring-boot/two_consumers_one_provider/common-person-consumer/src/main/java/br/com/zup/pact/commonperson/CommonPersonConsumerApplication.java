51810c6b078bf8a087097cdbef4652d014972159
Hader Araujo <hader.araujo@zup.com.br>
2020-08-26 18:27:34 -0300

    common-person-client

---
+++ example/java/spring-boot/two_consumers_one_provider/common-person-consumer/src/main/java/br/com/zup/pact/commonperson/CommonPersonConsumerApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.pact.commonperson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonPersonConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonPersonConsumerApplication.class, args);
+    }
+
+}
