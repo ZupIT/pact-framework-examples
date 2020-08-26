2f0ad9580163d17114a85035c3af2c2a804e246b
Hader Araujo <hader.araujo@zup.com.br>
2020-08-26 09:43:05 -0300

    projects name changed

--- example/java/spring-boot/one_consumer_one_provider/consumer/src/main/java/br/com/zup/pact/consumer/ConsumerApplication.java
+++ example/java/spring-boot/one_consumer_one_provider/consumer/src/main/java/br/com/zup/pact/consumer/ConsumerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ClientApiApplication {
+public class ConsumerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ClientApiApplication.class, args);
+        SpringApplication.run(ConsumerApplication.class, args);
     }
 
 }
--- example/java/spring-boot/one_consumer_one_provider/provider/src/main/java/br/com/zup/pact/provider/ProviderApplication.java
+++ example/java/spring-boot/one_consumer_one_provider/provider/src/main/java/br/com/zup/pact/provider/ProviderApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AccountApiApplication {
+public class ProviderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AccountApiApplication.class, args);
+        SpringApplication.run(ProviderApplication.class, args);
     }
 
 }
