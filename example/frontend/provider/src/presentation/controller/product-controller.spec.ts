82d792116d8cea4568f22f962f42cc7e1e9506f9
Tony Augusto <tony.silva@zup.com.br>
2020-08-31 15:04:15 -0300

    test: should return 200 if product exists

--- example/frontend/provider/src/presentation/controller/product-controller.spec.ts
+++ example/frontend/provider/src/presentation/controller/product-controller.spec.ts
@@ -1,18 +1,54 @@
-import productController from "./product-controller";
-import { HttpRequest } from "../helpers/http";
-import { badRequest } from "../helpers/http-errors";
+import { ProductController } from "./product-controller";
+import { HttpRequest } from "../protocols/http";
+import { badRequest, success } from "../helpers/http-helpers";
 import { MissingParamError } from "../errors/missing-param";
+import { Product } from "../../domain/product";
+import { Repository } from "../../repository/product-repository";
 
+
+const makeFakeProduct = (): Product => {
+  return new Product(1, 'any_type', 'any_name')
+}
+
+const makeProductRepository = (): any => {
+  class ProductRepositoryStub implements Repository {
+    async getById(id: number): Promise<Product> {
+      return new Promise(resolve => resolve(makeFakeProduct()))
+    }
+
+    async getAll(): Promise<Product[]> {
+      return null
+    }
+  }
+  return new ProductRepositoryStub()
+}
+
+const makeSut = (): any => {
+  const sut = new ProductController(makeProductRepository())
+  return { sut }
+}
 
 describe('Product Controller', () => {
   describe('get product by id', () => {
     it('should return 400 if no id is provided', async () => {
+      const { sut } = makeSut()
       const httpRequest: HttpRequest = {
         body: 'any_body'
       }
-      const httpResponse = await productController.getById(httpRequest)
+      const httpResponse = await sut.getById(httpRequest)
       expect(httpResponse).toEqual(badRequest(new MissingParamError('id')))
     });
+
+    it('should return 200 if product exists', async () => {
+      const { sut } = makeSut()
+      const httpRequest: HttpRequest = {
+        params: 1,
+        body: 'any_body'
+      }
+      const httpResponse = await sut.getById(httpRequest)
+      expect(httpResponse).toEqual(success(makeFakeProduct()))
+    });
+
+
   });
-  
 });
