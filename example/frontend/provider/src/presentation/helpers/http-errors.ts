2579d003d8d7c980162100ce9724af9ae88033fe
Tony Augusto <tony.silva@zup.com.br>
2020-08-31 15:05:44 -0300

    refactor: rename http-helpers

--- example/frontend/provider/src/presentation/helpers/http-errors.ts
+++
@@ -1,6 +0,0 @@
-import { HttpResponse } from "./http";
-
-export const badRequest = (error: Error): HttpResponse => ({
-  statusCode: 400,
-  body: error
-})
---
+++ example/frontend/provider/src/presentation/helpers/http-helpers.ts
@@ -0,0 +1,11 @@
+import { HttpResponse } from "../protocols/http";
+
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: error
+})
+
+export const success = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data
+})
