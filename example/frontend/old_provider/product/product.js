b7c5428b86ef9ea4f7878f914014b83acc6c8584
Iann Santos <iann.ferreira@zup.com.br>
2020-09-02 17:38:35 -0300

    remove: unnecessary old_provider

--- example/frontend/old_provider/product/product.js
+++
@@ -1,9 +0,0 @@
-class Product {
-  constructor(id, type, name) {
-    this.id = id;
-    this.type = type;
-    this.name = name;
-  }
-}
-
-module.exports = Product;
--- example/frontend/old_provider/product/product.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const controller = require("./product.controller");
-
-router.get("/api/product/:id", controller.getById);
-router.get("/api/products", controller.getAll);
-
-module.exports = router;
