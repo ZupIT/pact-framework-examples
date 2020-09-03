f7292382a3e1dc5221625309b55f09cc6f31d167
Tony Augusto <tony.silva@zup.com.br>
2020-09-03 15:40:29 -0300

    refactor: add products in the list before each test

--- example/frontend/provider/src/repository/product/product-repository.spec.ts
+++ example/frontend/provider/src/repository/product/product-repository.spec.ts
@@ -1,6 +1,18 @@
 import productRepository from './product-repository'
+import { Product } from '../../domain/product';
 
 describe('Product Repository', () => {
+
+  beforeEach(() => {
+    productRepository.setProducts(new Map([
+      [1, new Product(1, "CREDIT_CARD", "Gem Visa")],
+      [2, new Product(2, "CREDIT_CARD", "28 Degrees")],
+      [3, new Product(3, "PERSONAL_LOAN", "MyFlexiPay")],
+      [4, new Product(4, "CREDIT_CARD", "MyFlexiPayCredit")],
+      [5, new Product(5, "PERSONAL_LOAN", "MyFlexiPay")],
+    ]))
+  })
+
   it('should return a list of products', async () => {
     const products = await productRepository.getAll()
     expect(products).toBeTruthy()
