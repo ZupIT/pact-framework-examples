5aaadbac975a9f4dbc0d063d4988d6a8902d058d
Tony Augusto <tony.silva@zup.com.br>
2020-09-04 15:41:03 -0300

    refactor: move app.close() to finally block

--- example/node/two_consumers_one_propvider/account-api/src/tests/VerifyPact.spec.ts
+++ example/node/two_consumers_one_propvider/account-api/src/tests/VerifyPact.spec.ts
@@ -16,10 +16,6 @@ describe('Pact verification', () => {
       );
   });
 
-  afterAll(async () => {
-    app.close();
-  });
-
   it('checking if provider agrees with consumer', async () => {
     const verify = new Verifier({
       providerBaseUrl: APP_URL,
@@ -32,6 +28,7 @@ describe('Pact verification', () => {
     await verify
       .verifyProvider()
       .then(() => console.log('Pact verification complete'))
-      .catch(err => console.log(err));
+      .catch(err => console.log(err))
+      .finally(() => app.close());
   });
 });
