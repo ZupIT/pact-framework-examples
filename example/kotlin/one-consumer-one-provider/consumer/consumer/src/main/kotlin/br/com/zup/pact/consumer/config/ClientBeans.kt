6234892a993c409f2260a053786d30b7a22d8481
Tony Augusto <tony.silva@zup.com.br>
2020-09-16 14:45:27 -0300

    create config package and files

---
+++ example/kotlin/one-consumer-one-provider/consumer/consumer/src/main/kotlin/br/com/zup/pact/consumer/config/ClientBeans.kt
@@ -0,0 +1,19 @@
+package br.com.zup.pact.consumer.config
+
+import org.springframework.boot.json.GsonJsonParser
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.client.RestTemplate
+
+@Configuration
+class ClientBeans {
+    @Bean
+    fun gson(): GsonJsonParser {
+        return GsonJsonParser()
+    }
+
+    @Bean
+    fun getRestTemplate(): RestTemplate {
+        return RestTemplate()
+    }
+}
