18babfd9db83f33522e04009be157cdf1a6c7d62
Tony Augusto <tony.silva@zup.com.br>
2020-09-17 16:47:08 -0300

    refactor Client.kt

--- example/kotlin/one-consumer-one-provider/consumer/consumer/src/main/kotlin/br/com/zup/pact/consumer/entity/Client.kt
+++ example/kotlin/one-consumer-one-provider/consumer/consumer/src/main/kotlin/br/com/zup/pact/consumer/entity/Client.kt
@@ -1,25 +1,21 @@
 package br.com.zup.pact.consumer.entity
 
 import br.com.zup.pact.consumer.dto.ClientDetailsDTO
-import java.util.*
 
-class Client (
+data class Client (
         private val id: Int,
         private val accountId: Int,
         private val name: String,
         private val finalName: String,
         private val age: Int )
 {
-    fun fromEntityToDto(client: Client): ClientDetailsDTO? {
-        if (Objects.nonNull(client)) {
-            return ClientDetailsDTO(
-                    id = client.id,
-                    accountId = client.accountId,
-                    age = client.age,
-                    name = client.name,
-                    finalName = client.finalName
-            )
-        }
-        return null
+    fun fromEntityToDto(): ClientDetailsDTO {
+        return ClientDetailsDTO(
+                id = id,
+                accountId = accountId,
+                age = age,
+                name = name,
+                finalName = finalName
+        )
     }
 }
