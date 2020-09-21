6dd8b78883aed54ade711ff0b4d004dcab3a86bc
Tony Augusto <tony.silva@zup.com.br>
2020-09-21 15:52:06 -0300

    test: new AccountStub class should has 10 AccountEntity

--- example/kotlin/one-consumer-one-provider/provider/src/main/kotlin/br/com/zup/pact/provider/stub/AccountStub.kt
+++ example/kotlin/one-consumer-one-provider/provider/src/main/kotlin/br/com/zup/pact/provider/stub/AccountStub.kt
@@ -4,6 +4,7 @@ import br.com.zup.pact.provider.dto.AccountDetailsDTO
 import br.com.zup.pact.provider.entity.AccountEntity
 import br.com.zup.pact.provider.enum.AccountType
 import org.springframework.stereotype.Component
+import javax.annotation.PostConstruct
 
 @Component
 class AccountStub {
@@ -12,6 +13,14 @@ class AccountStub {
     private val initialAccountType = AccountType.COMMON
 
     val accounts = mutableMapOf<Int, AccountEntity>()
+
+    @PostConstruct
+    private fun createStubs() {
+        accounts.putAll(
+                (1..numberOfStubs).map { AccountEntity(it, it, initialBalance, initialAccountType) }
+                        .associateBy({ it.accountId }, { it })
+        )
+    }
 
     fun getAllStubsDTOFormat(): List<AccountDetailsDTO> {
         return accounts.values.map(AccountEntity::toAccountDetailsDTO)
---
+++ example/kotlin/one-consumer-one-provider/provider/src/test/kotlin/br/com/zup/pact/provider/stub/AccountStubTest.kt
@@ -0,0 +1,31 @@
+package br.com.zup.pact.provider.stub
+
+import br.com.zup.pact.provider.entity.AccountEntity
+import br.com.zup.pact.provider.enum.AccountType
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AccountStubTest {
+
+    @Autowired
+    private lateinit var accountStub: AccountStub
+
+    @Test
+    fun `New AccountStub class should has 10 AccountEntity`() {
+        val numberOfStubs = 10
+        val initialBalance = 100.0
+        val initialAccountType = AccountType.COMMON
+
+        val result: Map<Int, AccountEntity> = accountStub.accounts
+
+        Assertions.assertThat(result).hasSize(numberOfStubs)
+
+        (1..numberOfStubs).forEach { id ->
+            Assertions.assertThat(AccountEntity(id, id, initialBalance, initialAccountType))
+                    .isEqualTo(result[id])
+        }
+    }
+}
