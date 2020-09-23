0c76bdfe0113bea3d2a59d34ceb6798254c513ca
Iann Santos <iann.ferreira@zup.com.br>
2020-09-23 15:34:34 -0300

    feat: add unit test to AccountService and refactor code

---
+++ example/node/one_consumer_one_provider/client-api/src/services/AccountService.spec.ts
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { AccountService } from './AccountService';
+
+jest.mock('axios');
+
+describe('AccountService', () => {
+  let accountService: AccountService;
+
+  beforeAll(() => {
+    accountService = new AccountService();
+  });
+
+  it('should get account when entering an existing ID', async () => {
+    const mockedAxios = axios as jest.Mocked<typeof axios>;
+    const mockValue = { data: { accountID: 1, clientID: 1, balance: 100 } };
+    mockedAxios.get.mockImplementationOnce(() => Promise.resolve(mockValue));
+
+    const response = await accountService.getBalanceByClientID(1);
+
+    expect(response).toEqual(mockValue.data);
+  });
+
+  it('should get an error when entering a non-existing ID', async () => {
+    await expect(
+      accountService.getBalanceByClientID(919119),
+    ).rejects.toThrowError("Client doesn't exist");
+  });
+});
