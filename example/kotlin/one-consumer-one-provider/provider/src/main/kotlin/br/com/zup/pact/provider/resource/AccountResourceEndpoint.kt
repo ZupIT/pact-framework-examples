f6a8483beaefd2f92e91d604daf75ada507e0f15
Tony Augusto <tony.silva@zup.com.br>
2020-09-25 14:50:55 -0300

    refactor: remove swagger dependencies and configs

--- example/kotlin/one-consumer-one-provider/provider/src/main/kotlin/br/com/zup/pact/provider/resource/AccountResourceEndpoint.kt
+++ example/kotlin/one-consumer-one-provider/provider/src/main/kotlin/br/com/zup/pact/provider/resource/AccountResourceEndpoint.kt
@@ -3,9 +3,6 @@ package br.com.zup.pact.provider.resource
 import br.com.zup.pact.provider.dto.AccountDetailsDTO
 import br.com.zup.pact.provider.dto.BalanceDTO
 import br.com.zup.pact.provider.service.AccountService
-import io.swagger.annotations.ApiOperation
-import io.swagger.annotations.ApiResponse
-import io.swagger.annotations.ApiResponses
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -18,26 +15,13 @@ import java.util.*
 @RequestMapping("/v1/accounts")
 class AccountResourceEndpoint(val accountService: AccountService) {
     @GetMapping("/{clientId}")
-    @ApiOperation(value = "Get Account Details by client id", response = AccountDetailsDTO::class)
-    @ApiResponses(
-            ApiResponse(code = 200, message = "AccountReturned", response = AccountDetailsDTO::class),
-            ApiResponse(code = 404, message = "Account not found")
-    )
     fun getAccountDetailsByClientId(@PathVariable("clientId") clientId: Int): ResponseEntity<AccountDetailsDTO>{
-        val accountDetailsDTO: Optional<AccountDetailsDTO> = accountService.getAccountDetailsByClientId(clientId);
+        val accountDetailsDTO: Optional<AccountDetailsDTO> = accountService.getAccountDetailsByClientId(clientId)
         return if (accountDetailsDTO.isPresent) ResponseEntity(accountDetailsDTO.get(), HttpStatus.OK)
             else ResponseEntity(HttpStatus.NOT_FOUND)
     }
 
     @GetMapping
-    @ApiOperation(notes = "Return all Accounts",
-            value = "Get all Accounts Details",
-            nickname = "getAll",
-            response = AccountDetailsDTO::class)
-    @ApiResponses(
-            ApiResponse(code = 200, message = "AccountReturned", response = AccountDetailsDTO::class),
-            ApiResponse(code = 404, message = "Account not found")
-    )
     fun getAll(): ResponseEntity<List<AccountDetailsDTO>> {
         val allAccounts: List<AccountDetailsDTO> = accountService.getAll()
         return if (allAccounts.isNotEmpty()) ResponseEntity(allAccounts, HttpStatus.OK)
@@ -45,14 +29,6 @@ class AccountResourceEndpoint(val accountService: AccountService) {
     }
 
     @GetMapping("/balance/{clientId}")
-    @ApiOperation(notes = "Return balance account of clientId",
-            value = "Get balance account of clientId",
-            nickname = "getBalanceOfClientId",
-            response = BalanceDTO::class)
-    @ApiResponses(
-            ApiResponse(code = 200, message = "Balance Returned", response = BalanceDTO::class),
-            ApiResponse(code = 404, message = "Client Not Found")
-    )
     fun getBalanceByClientId(@PathVariable("clientId") clientId: Int): ResponseEntity<BalanceDTO> {
         val balanceDTO: Optional<BalanceDTO> = accountService.getBalanceByClientId(clientId)
         return if (balanceDTO.isPresent) ResponseEntity(balanceDTO.get(), HttpStatus.OK)
