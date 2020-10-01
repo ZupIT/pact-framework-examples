00b01df03b6dc42a82d4119a96fca3e9f6d30fca
Tony Augusto <tony.silva@zup.com.br>
2020-10-01 11:20:43 -0300

    feat: getClientDetails should return a ClientDetails from repository

---
+++ example/kotlin/one-consumer-one-provider/consumer/src/main/kotlin/br/com/zup/pact/consumer/service/ClientServiceImpl.kt
@@ -0,0 +1,22 @@
+package br.com.zup.pact.consumer.service
+
+import br.com.zup.pact.consumer.dto.BalanceDTO
+import br.com.zup.pact.consumer.dto.ClientDetailsDTO
+import br.com.zup.pact.consumer.repository.ClientRepository
+import org.springframework.beans.factory.annotation.Autowired
+
+class ClientServiceImpl (
+        @Autowired val clientRepository: ClientRepository
+): ClientService {
+    override fun getClientDetails(clientId: Int): ClientDetailsDTO? {
+        return clientRepository.findByClientId(clientId)
+    }
+
+    override fun getAll(): List<ClientDetailsDTO> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getBalance(clientId: Int): BalanceDTO? {
+        TODO("Not yet implemented")
+    }
+}
---
+++ example/kotlin/one-consumer-one-provider/consumer/src/test/kotlin/br/com/zup/pact/consumer/service/ClientServiceImplTest.kt
@@ -0,0 +1,45 @@
+package br.com.zup.pact.consumer.service
+
+import br.com.zup.pact.consumer.dto.ClientDetailsDTO
+import br.com.zup.pact.consumer.repository.ClientRepository
+import io.mockk.clearMocks
+import io.mockk.every
+import io.mockk.junit5.MockKExtension
+import io.mockk.mockk
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+@ExtendWith(MockKExtension::class)
+class ClientServiceImplTest {
+    private val clientRepositoryMock: ClientRepository = mockk()
+    private val clientService: ClientService = ClientServiceImpl(clientRepositoryMock)
+
+    @BeforeEach
+    fun setUp() {
+        clearMocks(clientRepositoryMock)
+    }
+
+    @Test
+    fun `Method getClientDetails should return a ClientDetails from repository`() {
+        every { clientRepositoryMock.findByClientId(1) }
+                .returns(createClientDetails(1))
+
+        val expectedClientDetails = clientService.getClientDetails(1)
+
+        Assertions.assertThat(expectedClientDetails)
+                .isEqualTo(createClientDetails(1))
+    }
+
+    private fun createClientDetails(
+            clientId: Int,
+            accountId: Int = 1,
+            firstName: String = "any_first_name",
+            lastName: String = "any_last_name",
+            age: Int = 20
+    ) = ClientDetailsDTO(
+            clientId, accountId, firstName, lastName, age
+    )
+
+}
