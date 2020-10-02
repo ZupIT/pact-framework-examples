95aea39d1b1a69c2d9a04fdb963a456816005902
Tony Augusto <tony.silva@zup.com.br>
2020-10-02 15:09:16 -0300

    feat: getBalance by acccountId returns a body with BalanceDTO

--- example/kotlin/one-consumer-one-provider/consumer/src/main/kotlin/br/com/zup/pact/consumer/integration/account/service/AccountIntegrationServiceImpl.kt
+++ example/kotlin/one-consumer-one-provider/consumer/src/main/kotlin/br/com/zup/pact/consumer/integration/account/service/AccountIntegrationServiceImpl.kt
@@ -3,28 +3,22 @@ package br.com.zup.pact.consumer.integration.account.service
 import br.com.zup.pact.consumer.dto.BalanceDTO
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.web.client.RestTemplateBuilder
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.PropertySource
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
 
 @Service
 class AccountIntegrationServiceImpl (
-        @Value("\${integration.account.balance.url}") private val accountBalanceUrl: String
+        @Value("\${integration.account.balance.url}") private val accountBalanceUrl: String,
+        @Autowired private val restTemplate: RestTemplate
 ): AccountIntegrationService {
 
-    @Bean
-    fun restTemplate(): RestTemplate {
-        return RestTemplateBuilder().build()
-    }
-
-    @Autowired
-    private lateinit var restTemplate: RestTemplate
-
     override fun getBalance(accountId: Int): BalanceDTO? {
-        val responseEntity: ResponseEntity<BalanceDTO> = restTemplate.getForEntity(accountBalanceUrl, BalanceDTO::class.java, accountId)
+        val responseEntity: ResponseEntity<BalanceDTO> = restTemplate.getForEntity(
+                accountBalanceUrl,
+                BalanceDTO::class.java,
+                accountId
+        )
         return responseEntity.body
     }
 }
