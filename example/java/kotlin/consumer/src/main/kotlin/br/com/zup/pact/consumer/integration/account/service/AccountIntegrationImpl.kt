ef87e22d607959a6afb45c6135420f70ba98d6aa
Hader Araujo <hader.araujo@zup.com.br>
2020-10-16 09:58:07 -0300

    AccountIntegrationImplTest done

--- example/java/kotlin/consumer/src/main/kotlin/br/com/zup/pact/consumer/integration/account/service/AccountIntegrationImpl.kt
+++ example/java/kotlin/consumer/src/main/kotlin/br/com/zup/pact/consumer/integration/account/service/AccountIntegrationImpl.kt
@@ -4,24 +4,11 @@ import br.com.zup.pact.consumer.dto.BalanceDTO
 import br.com.zup.pact.provider.resource.AccountIdRequest
 import br.com.zup.pact.provider.resource.AccountResourceGrpc
 import br.com.zup.pact.provider.resource.AccountResponse
-import io.grpc.ManagedChannel
-import io.grpc.ManagedChannelBuilder
-import org.springframework.beans.factory.annotation.Value
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import javax.annotation.PreDestroy
-
 
 @Service
-class AccountIntegrationImpl(
-        @Value("\${integration.account.balance.address}")
-        private val accountBalanceAddress: String,
-        @Value("\${integration.account.balance.port}")
-        private val accountBalancePort: Int
-) : AccountIntegrationService {
-
-    val channel: ManagedChannel = ManagedChannelBuilder.forAddress(accountBalanceAddress, accountBalancePort)
-            .usePlaintext()
-            .build()
+class AccountIntegrationImpl(@Autowired private val grpcService: GrpcService) : AccountIntegrationService {
 
     override fun getBalance(accountId: Int): BalanceDTO? {
 
@@ -29,7 +16,8 @@ class AccountIntegrationImpl(
                 .setAccountId(accountId)
                 .build()
 
-        val stub: AccountResourceGrpc.AccountResourceBlockingStub = AccountResourceGrpc.newBlockingStub(channel)
+        val stub: AccountResourceGrpc.AccountResourceBlockingStub = grpcService.getAccountResourceBlockingStub()
+
         val balanceResponse: AccountResponse = stub.findById(balanceRequest)
 
         return BalanceDTO(
@@ -37,11 +25,6 @@ class AccountIntegrationImpl(
                 balance = balanceResponse.balance
         )
 
-    }
-
-    @PreDestroy
-    private fun destroy() {
-        channel.shutdown()
     }
 
 }
