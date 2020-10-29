8d716be0c0fa0af4a8f787092885947dca7c473a
Tony Augusto <tony.silva@zup.com.br>
2020-10-29 14:10:38 -0300

    feat: should getAll returns all products from the service

---
+++ example/kotlin/micronaut-grpc/provider/src/test/kotlin/br/com/zup/pact/provider/resource/ProductResourceTest.kt
@@ -0,0 +1,58 @@
+package br.com.zup.pact.provider.resource
+
+import br.com.zup.pact.provider.EmptyRequest
+import br.com.zup.pact.provider.ProductResponse
+import br.com.zup.pact.provider.dto.ProductDTO
+import br.com.zup.pact.provider.enums.PaymentMethod
+import br.com.zup.pact.provider.service.ProductService
+import io.grpc.stub.StreamObserver
+import io.mockk.*
+import io.mockk.junit5.MockKExtension
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+@ExtendWith(MockKExtension::class)
+class ProductResourceTest {
+
+    private val productServiceMock: ProductService = mockk()
+    private val productResource: ProductResource = ProductResource(productServiceMock)
+
+    @Test
+    fun `should getAll returns all products from the service`() {
+
+        every { productServiceMock.getAll() }
+                .returns(listOf(
+                        ProductDTO(1, "any product", 150.0, 12,
+                                PaymentMethod.DEFERRED_PAYMENT
+                        ),
+                        ProductDTO(2, "another product", 134.0, 6,
+                                PaymentMethod.CASH_PAYMENT
+                        )
+                ))
+
+        val responseObserver: StreamObserver<ProductResponse> = mockk()
+        every { responseObserver.onNext(any()) } just Runs
+        every { responseObserver.onCompleted() } just Runs
+
+        productResource.getAll(
+                EmptyRequest.newBuilder().build(),
+                responseObserver
+        )
+
+        verify(exactly = 1) {
+            responseObserver.onNext(
+                    ProductResponse.newBuilder()
+                            .setProductId(1)
+                            .setName("any product")
+                            .setPrice(150.0)
+                            .setQuantity(12)
+                            .setPaymentMethod(PaymentMethod.DEFERRED_PAYMENT.toString())
+                            .build()
+            )
+        }
+
+        verify(exactly = 1) {
+            responseObserver.onCompleted()
+        }
+    }
+}
