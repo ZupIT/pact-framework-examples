a196aebd183a341bd427fe10073970a9ff4be3f5
Tony Augusto <tony.silva@zup.com.br>
2020-10-31 17:56:32 -0300

    test: should getAll products returns a list ProductDTO

---
+++ example/kotlin/micronaut-grpc/consumer/src/test/kotlin/br/com/zup/pact/consumer/integration/product/service/ProductIntegrationImplTest.kt
@@ -0,0 +1,62 @@
+package br.com.zup.pact.consumer.integration.product.service
+
+import br.com.zup.pact.consumer.dto.ProductDTO
+import br.com.zup.pact.consumer.enums.PaymentMethod
+import br.com.zup.pact.provider.ProductResponse
+import br.com.zup.pact.provider.ProductServiceGrpc
+import io.mockk.clearMocks
+import io.mockk.every
+import io.mockk.junit5.MockKExtension
+import io.mockk.mockk
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+@ExtendWith(MockKExtension::class)
+class ProductIntegrationImplTest {
+
+    private val grpcServiceMock: GrpcServiceIntegration = mockk()
+    private val productIntegrationImpl: ProductIntegration = ProductIntegrationImpl(grpcServiceMock)
+
+    @BeforeEach
+    fun setUp() {
+        clearMocks(grpcServiceMock)
+    }
+
+    @Test
+    fun `should getAll products returns a list ProductDTO`() {
+
+        val stubMock: ProductServiceGrpc.ProductServiceBlockingStub = mockk()
+
+        every { stubMock.getAll(any()) }
+                .returns(
+                        mutableListOf<ProductResponse>(
+                                ProductResponse.newBuilder()
+                                        .setProductId(1)
+                                        .setName("Any product")
+                                        .setPrice(100.0)
+                                        .setQuantity(3)
+                                        .setPaymentMethod(PaymentMethod.CASH_PAYMENT.toString())
+                                        .build()
+                        ).listIterator()
+                )
+
+        every { grpcServiceMock.getProductsResourceBlockingStub() }
+                .returns(stubMock)
+
+        val actualReturn = productIntegrationImpl.getAll()
+
+        Assertions.assertThat(actualReturn).isEqualTo(
+                listOf(
+                        ProductDTO(
+                                1,
+                                "Any product",
+                                100.0,
+                                3,
+                                PaymentMethod.CASH_PAYMENT
+                        )
+                )
+        )
+    }
+}
